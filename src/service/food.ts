94844c63d0fd27f2160347fa1b5f17fbbcadf928
Timyu0527 <st970387@gmail.com>
2023-02-10 22:43:33 +0800

    remove getCities

--- src/service/food.ts
+++ src/service/food.ts
@@ -1,12 +1,4 @@
-<<<<<<< HEAD:src/firebase/utility.ts
 import { collection, getDocs, Firestore, DocumentData,addDoc,where,query, Query, deleteDoc } from 'firebase/firestore';
-
-export async function getCities(db: Firestore, collectionName: string){
-    const querySnapshot = await getDocs(collection(db, collectionName));
-    querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()['test']}`);
-    });
-}
 
 export async function addRestaurant(db: Firestore, name:string,tel:string,image:string,guildId: string,userId:string) {
     try{
@@ -80,31 +72,4 @@ export async function orderResult(db:Firestore,guildId:string,userId:string) {
         console.error('order result error: ',err);
         throw err;
     } 
-=======
-import { collection, getDocs, Firestore, DocumentData,addDoc,where,query, Query } from 'firebase/firestore'
-
-export async function addRestaurant(db: Firestore, name:string,tel:string,image:string,guildId: string,userId:string) {
-    await addDoc(collection(db,'restaurant'), {
-        userId: userId,
-        guildId: guildId,
-        name: name,
-        tel: tel,
-        image: image
-    })
-}
-
-export async function getRestaurant(db: Firestore,name:string,guildId: string,userId:string) {
-    let q:Query<DocumentData>
-    if(guildId==''){
-        q = query(collection(db, 'restaurant'), where('userId', '==', userId), where('name', '==', name))
-    }else{
-        q = query(collection(db, 'restaurant'),where('guildId','==',guildId),where('name','==',name))
-    }
-    let querySnapshot=await getDocs(q)
-    let result:DocumentData[]=[]
-    querySnapshot.forEach((doc)=>{
-        result.push(doc.data())
-    })
-    return result
->>>>>>> main:src/service/food.ts
 }
